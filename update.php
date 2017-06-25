77136a15ada3e24bac699dfcdce4c9d722af801b
Carlos Ayala <ceal1818@gmail.com>
2017-06-26 01:18:42 +0200

    Primer commit del proyecto.

---
+++ update.php
@@ -0,0 +1,119 @@
+<?php
+    require 'database.php';
+ 
+    $id = null;
+    if ( !empty($_GET['id'])) {
+        $id = $_REQUEST['id'];
+    }
+     
+    if ( null==$id ) {
+        header("Location: index.php");
+    }
+     
+    if ( !empty($_POST)) {
+        // keep track validation errors
+        $nameError = null;
+        $emailError = null;
+         
+        // keep track post values
+        $firstName = $_POST['first_name'];
+        $lastName = $_POST['last_name'];
+        $email = $_POST['email'];
+         
+        // validate input
+        $valid = true;
+        if (empty($firstName)) {
+            $nameError = 'Please enter First name';
+            $valid = false;
+        }
+         
+        if (empty($lastName)) {
+            $nameError = 'Please enter Last name';
+            $valid = false;
+        }
+
+        if (empty($email)) {
+            $emailError = 'Please enter Email Address';
+            $valid = false;
+        } else if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
+            $emailError = 'Please enter a valid Email Address';
+            $valid = false;
+        }
+         
+        // update data
+        if ($valid) {
+            $pdo = Database::connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "UPDATE users  set first_name = ?, last_name = ?, email = ? WHERE id = ?";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($firstName,$lastName,$email,$id));
+            Database::disconnect();
+            header("Location: index.php");
+        }
+    } else {
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "SELECT * FROM users where id = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        $data = $q->fetch(PDO::FETCH_ASSOC);
+        $firstName = $data['first_name'];
+        $lastName = $data['last_name'];
+        $email = $data['email'];
+        Database::disconnect();
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+ 
+<body>
+    <div class="container">
+     
+                <div class="span10 offset1">
+                    <div class="row">
+                        <h3>Update a Customer</h3>
+                    </div>
+             
+                    <form class="form-horizontal" action="update.php?id=<?php echo $id?>" method="post">
+                      <div class="control-group <?php echo !empty($nameError)?'error':'';?>">
+                        <label class="control-label">First Name</label>
+                        <div class="controls">
+                            <input name="first_name" type="text"  placeholder="First Name" value="<?php echo !empty($firstName)?$firstName:'';?>">
+                            <?php if (!empty($nameError)): ?>
+                                <span class="help-inline"><?php echo $nameError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($nameError)?'error':'';?>">
+                        <label class="control-label">Last Name</label>
+                        <div class="controls">
+                            <input name="last_name" type="text"  placeholder="Last Name" value="<?php echo !empty($lastName)?$lastName:'';?>">
+                            <?php if (!empty($nameError)): ?>
+                                <span class="help-inline"><?php echo $nameError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($emailError)?'error':'';?>">
+                        <label class="control-label">Email Address</label>
+                        <div class="controls">
+                            <input name="email" type="text" placeholder="Email Address" value="<?php echo !empty($email)?$email:'';?>">
+                            <?php if (!empty($emailError)): ?>
+                                <span class="help-inline"><?php echo $emailError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+                      <div class="form-actions">
+                          <button type="submit" class="btn btn-success">Update</button>
+                          <a class="btn" href="index.php">Back</a>
+                        </div>
+                    </form>
+                </div>
+                 
+    </div> <!-- /container -->
+  </body>
+</html>
